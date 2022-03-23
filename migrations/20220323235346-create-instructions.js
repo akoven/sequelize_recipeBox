8a155dc28593835eead97e9426f65ae98ec11ae8
Attiya <adarensburg@live.com>
2022-03-23 18:58:11 -0500

    phase 5 finished

---
+++ migrations/20220323235346-create-instructions.js
@@ -0,0 +1,32 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('instructions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      specification: {
+        type: Sequelize.TEXT,
+        allowNull: false
+      },
+      listOrder: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('instructions');
+  }
+};
---
+++ models/instructions.js
@@ -0,0 +1,11 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const instructions = sequelize.define('instructions', {
+    specification: DataTypes.TEXT,
+    listOrder: DataTypes.INTEGER
+  }, {});
+  instructions.associate = function(models) {
+    // associations can be defined here
+  };
+  return instructions;
+};
